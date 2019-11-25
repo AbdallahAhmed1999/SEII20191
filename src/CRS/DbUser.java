abd49c542c246dde4513556538e1379a9b253b9f
aashgar <aashgar@CASHGAR.iugaza.edu>
2019-11-25 10:46:34 +0200

    All Done ,,,

---
+++ src/CRS/DbUser.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CRS;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+/**
+ *
+ * @author aashgar
+ */
+public class DbUser {
+   private Statement aStatement;
+   public boolean verifyUser(String userName, String password){
+       boolean isFound=false;
+       DbConnection aDbConnection = DbConnection.getaDbConnection();
+       try {
+           this.aStatement = aDbConnection.getStatement();
+       
+       String sql = "Select * From Users Where userName='" + userName +
+               "' And Password='" + password + "'";
+       ResultSet rs = this.aStatement.executeQuery(sql);
+       if(rs.next())
+           isFound = true;
+       } catch (Exception ex) {
+           ex.printStackTrace();
+       }
+       return isFound;
+   }
+}
